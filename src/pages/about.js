d57f2a3f3c9b1f7005c70863082dfad3e6097a2e
Tahsin Yazkan <tahsinyazkan@gmail.com>
2019-02-06 11:41:10 +0300

    First Commit

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from '../components/header'
+export default () => (
+  <div style={{color: `teal`}}>
+    <Header headerText="Bout me"/>
+    <Header headerText="Props are like parameters to functions"/>
+    <p>Such amazing me!</p>
+  </div>
+)
